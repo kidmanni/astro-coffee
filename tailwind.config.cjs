2fbd36249212f4d2c7fe7f338ada53a37d6b516b
kidmanni <76219515+kidmanni@users.noreply.github.com>
2025-06-17 19:08:41 +0000

    Initial Astro+Tailwind v4 scaffold

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js}'],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#A47449',
+        secondary: '#D9B28E'
+      }
+    }
+  },
   plugins: []
 };
